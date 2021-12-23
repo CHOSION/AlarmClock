8e5109224299a5542cfb5010040724103bcb1d32
CHOSION <chosion0107@gmail.com>
2021-12-23 18:14:39 +0900

    Initial commit

---
+++ AlarmClock/AlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmClockApp.swift
+//  AlarmClock
+//
+//  Created by 조세연 on 2021/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmClock/Time.swift
@@ -0,0 +1,14 @@
+//
+//  Time.swift
+//  AlarmClock
+//
+//  Created by 조세연 on 2021/12/23.
+//
+
+import Foundation
+
+struct Time {
+    var ss: Int
+    var mm: Int
+    var hh: Int
+}
