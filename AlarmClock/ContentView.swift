784d177c19d0b68ea7e59837bf4e45575a1c2d2b
CHOSION <chosion0107@gmail.com>
2021-12-24 16:43:19 +0900

    211224

--- AlarmClock/ContentView.swift
+++ AlarmClock/ContentView.swift
@@ -171,39 +171,3 @@ struct FormatFomt: ViewModifier {
             .padding(.top, 10)
     }
 }
-
-struct ContentView: View {
-    @State var selectedDate = Date()
-
-    static let formatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.setLocalizedDateFormatFromTemplate("yyMMddhhmm")
-        return formatter
-    }()
-
-    var body: some View {
-        VStack {
-            Text("Selected date: \(selectedDate, formatter: Self.formatter)")
-            Button("Show action sheet") {
-                self.showDatePickerAlert()
-            }
-        }
-    }
-
-    func showDatePickerAlert() {
-        let alertVC = UIAlertController(title: "\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
-        let datePicker: UIDatePicker = UIDatePicker()
-        alertVC.view.addSubview(datePicker)
-
-        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
-            self.selectedDate = datePicker.date
-        }
-        alertVC.addAction(okAction)
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        alertVC.addAction(cancelAction)
-
-        if let viewController = UIApplication.shared.windows.first?.rootViewController {
-            viewController.present(alertVC, animated: true, completion: nil)
-        }
-    }
-}
