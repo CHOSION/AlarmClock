2e334090649e06f9da5d59c7ae7f38b1a2c06982
CHOSION <chosion0107@gmail.com>
2021-12-24 16:42:53 +0900

    211224

--- AlarmClock/ContentView.swift
+++ AlarmClock/ContentView.swift
@@ -11,6 +11,7 @@ import UserNotificationsUI
 struct ContentView: View {
     @State var isDark: Bool = false
     @State var is12h: Bool = false
+    
     var body: some View {
         Home(is12h: $is12h, isDark: $isDark)
             .navigationBarHidden(true)
@@ -33,6 +34,7 @@ struct Home: View {
         
     var body: some View {
         VStack {
+            
             HStack {
                 Text(Locale.current.localizedString(forRegionCode: Locale.current.regionCode!) ?? "")
                     .font(.largeTitle)
@@ -167,5 +169,41 @@ struct FormatFomt: ViewModifier {
             .foregroundColor(.primary)
             .cornerRadius(20)
             .padding(.top, 10)
+    }
+}
+
+struct ContentView: View {
+    @State var selectedDate = Date()
+
+    static let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("yyMMddhhmm")
+        return formatter
+    }()
+
+    var body: some View {
+        VStack {
+            Text("Selected date: \(selectedDate, formatter: Self.formatter)")
+            Button("Show action sheet") {
+                self.showDatePickerAlert()
+            }
+        }
+    }
+
+    func showDatePickerAlert() {
+        let alertVC = UIAlertController(title: "\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        let datePicker: UIDatePicker = UIDatePicker()
+        alertVC.view.addSubview(datePicker)
+
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.selectedDate = datePicker.date
+        }
+        alertVC.addAction(okAction)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertVC.addAction(cancelAction)
+
+        if let viewController = UIApplication.shared.windows.first?.rootViewController {
+            viewController.present(alertVC, animated: true, completion: nil)
+        }
     }
 }
